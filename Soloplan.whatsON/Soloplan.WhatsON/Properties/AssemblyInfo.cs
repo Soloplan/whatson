54e1aa9b56a1c40f205e59633e482b0084318ff9
Steffen Wilke <mail@steffen-wilke.de>
2019-01-28 13:13:41 +0100

    Add a shared assembly info for the basic whatsON projects.
    This will allow us to force the same version and copyright to all basic .dlls and executables.

--- Soloplan.whatsON/Soloplan.WhatsON/Properties/AssemblyInfo.cs
+++ Soloplan.whatsON/Soloplan.WhatsON/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Soloplan.WhatsON.CLI")]
-[assembly: AssemblyDescription("A Command Line Interface for the whatsON build and infrastructure monitoring tool.")]
+[assembly: AssemblyTitle("Soloplan.WhatsON")]
+[assembly: AssemblyDescription("The core library for the whatsON build and infrastructure monitoring tool.")]
