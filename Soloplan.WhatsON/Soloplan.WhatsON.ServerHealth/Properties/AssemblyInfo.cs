81932a25ed232792bb9bfeb393fff2d2e1150851
Steffen Wilke <mail@steffen-wilke.de>
2019-01-28 14:16:50 +0100

    Rename base path.

---
+++ Soloplan.WhatsON/Soloplan.WhatsON.ServerHealth/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Soloplan.WhatsON.ServerHealth")]
+[assembly: AssemblyDescription("The library that observers server health for the whatsON build and infrastructure monitoring tool.")]
