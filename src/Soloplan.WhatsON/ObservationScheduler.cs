6ba1019bef4aee5928cf5221caea8dbd7620617a
DGO <d.golda@interia.eu>
2019-04-16 16:20:37 +0200

    Add comments to ObservationScheduler. cleanup code - some function names no longer made sense.

--- src/Soloplan.WhatsON/ObservationScheduler.cs
+++ src/Soloplan.WhatsON/ObservationScheduler.cs
@@ -1,4 +1,11 @@
-﻿namespace Soloplan.WhatsON
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NumericConfigControlBuilder.cs" company="Soloplan GmbH">
+//   Copyright (c) Soloplan GmbH. All rights reserved.
+//    Licensed under the MIT License. See License-file in the project root for license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Soloplan.WhatsON
 {
   using System;
   using System.Collections.Generic;
@@ -6,9 +13,19 @@
   using System.Threading;
   using System.Threading.Tasks;
 
+  /// <summary>
+  /// Main class of application. Executes specialized plugins in desired intervals.
+  /// </summary>
   public class ObservationScheduler
   {
+    /// <summary>
+    /// Default interval for polling subjects for status.
+    /// </summary>
     private const int DefaultPollInterval = 5;
+
+    /// <summary>
+    /// List of currently observed subjects.
+    /// </summary>
     private readonly IList<ObservationSubject> observedSubjects;
 
     /// <summary>
@@ -16,13 +33,22 @@
     /// </summary>
     private bool stopping;
 
+    /// <summary>
+    /// Cancellation token used when all open connections should be forcefully closed.
+    /// </summary>
     private CancellationTokenSource cancellationTokenSource;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObservationScheduler"/> class.
+    /// </summary>
     public ObservationScheduler()
     {
       this.observedSubjects = new List<ObservationSubject>();
     }
 
+    /// <summary>
+    /// Fired when subjects status was polled.
+    /// </summary>
     public event EventHandler<Subject> StatusQueried;
 
     public bool Running
@@ -39,14 +65,22 @@
           observationSubject.Running = true;
         }
 
-        this.cancellationTokenSource = new CancellationTokenSource();
+        if (this.cancellationTokenSource == null || this.cancellationTokenSource.IsCancellationRequested)
+        {
+          this.cancellationTokenSource = new CancellationTokenSource();
+        }
+
         foreach (var observationSubject in this.observedSubjects)
         {
-          this.Observe(observationSubject, this.cancellationTokenSource.Token);
+          this.StartObserveSingle(observationSubject, this.cancellationTokenSource.Token);
         }
       }
     }
 
+    /// <summary>
+    /// Stops observation scheduler.
+    /// </summary>
+    /// <param name="force">If true forces all tasks to stop; otherwise allows tasks to finish their current run.</param>
     public void Stop(bool force)
     {
       this.Terminate(force);
@@ -68,6 +102,9 @@
       this.observedSubjects.Add(new ObservationSubject(subject, interval));
     }
 
+    /// <summary>
+    /// Stops observing all subjects. The scheduler should be stopped before calling this function.
+    /// </summary>
     public void UnobserveAll()
     {
       if (this.Running || this.stopping)
@@ -78,48 +115,41 @@
       this.observedSubjects.Clear();
     }
 
-    private async Task Observe(ObservationSubject subject, CancellationToken token)
+    /// <summary>
+    /// Observes single subject. Creates asynchronous loop.
+    /// </summary>
+    /// <param name="subject">Subject to observe.</param>
+    /// <param name="token">Cancellation token passed to subject.</param>
+    /// <remarks>See https://blogs.msdn.microsoft.com/benwilli/2016/06/30/asynchronous-infinite-loops-instead-of-timers/ for look idea description.</remarks>
+    /// <returns>Not used.</returns>
+    private async Task StartObserveSingle(ObservationSubject subject, CancellationToken token)
     {
       while (subject.Running)
       {
         if (DateTime.Now - subject.LastPoll > subject.Interval)
         {
-          try
+          await subject.Subject.QueryStatus(token);
+          subject.LastPoll = DateTime.Now;
+          if (subject.Running)
           {
-            await this.ObserveSingle(subject, token);
-          }
-          catch (Exception e)
-          {
-            // Todo: should be logged.
+            this.StatusQueried?.Invoke(this, subject.Subject);
           }
         }
 
-        try
+        var remainingOfInterval = subject.Interval - (DateTime.Now - subject.LastPoll);
+        if (remainingOfInterval.TotalMilliseconds > 0 && subject.Running)
         {
-          var remainingOfInterval = subject.Interval - (DateTime.Now - subject.LastPoll);
-          if (remainingOfInterval.TotalMilliseconds > 0 && subject.Running)
-          {
-            int milisecondsToWait = remainingOfInterval.TotalMilliseconds < int.MaxValue ? (int)remainingOfInterval.TotalMilliseconds : int.MaxValue;
-            await Task.Delay(milisecondsToWait, token);
-          }
-        }
-        catch (Exception e)
-        {
-          // Todo: should be logged.
+          int milisecondsToWait = remainingOfInterval.TotalMilliseconds < int.MaxValue ? (int)remainingOfInterval.TotalMilliseconds : int.MaxValue;
+          await Task.Delay(milisecondsToWait, token);
         }
       }
     }
 
-    private async Task ObserveSingle(ObservationSubject subject, CancellationToken token)
-    {
-      await subject.Subject.QueryStatus(token);
-      subject.LastPoll = DateTime.Now;
-      if (subject.Running)
-      {
-        this.StatusQueried?.Invoke(this, subject.Subject);
-      }
-    }
-
+    /// <summary>
+    /// Stops observation of subjects.
+    /// </summary>
+    /// <param name="force">If true cancellation token is used to cancel running observations;
+    /// otherwise the observation are informed that they should stop, but are allowed to run until they end current operation/finish waiting for next poll interval.</param>
     private void Terminate(bool force)
     {
       if (!this.Running)
@@ -146,20 +176,44 @@
       }
     }
 
+    /// <summary>
+    /// Helper class representing the subject being observed.
+    /// </summary>
     private class ObservationSubject
     {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ObservationSubject"/> class.
+      /// </summary>
+      /// <param name="subject">The subject which should be observed.</param>
+      /// <param name="interval">Observation interval.</param>
       public ObservationSubject(Subject subject, int interval)
       {
         this.Subject = subject;
         this.Interval = TimeSpan.FromSeconds(interval);
       }
 
+      /// <summary>
+      /// Gets or sets a value indicating whether the subject should currently be observed.
+      /// </summary>
+      /// <remarks>
+      /// The subjects loop may be still running even though this property is false. The subject should not send any events nor trigger new requests
+      /// when this is false.
+      /// </remarks>
       public bool Running { get; set; }
 
+      /// <summary>
+      /// Gets the subject being observed.
+      /// </summary>
       public Subject Subject { get; }
 
+      /// <summary>
+      /// Gets interval in which the subject should be polled for status.
+      /// </summary>
       public TimeSpan Interval { get; }
 
+      /// <summary>
+      /// Gets or sets date the subject was last polled.
+      /// </summary>
       public DateTime LastPoll { get; set; }
     }
   }
