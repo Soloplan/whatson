cca22a148c5e5fa6fe4d8e916be283a5cb212b5b
DGO <d.golda@interia.eu>
2019-05-30 16:21:24 +0200

    Add invert bool converters.

---
+++ src/Soloplan.WhatsON.GUI.Common/InvertBooleanConverter.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InvertBoolToVisibleConverter.cs" company="Soloplan GmbH">
+//   Copyright (c) Soloplan GmbH. All rights reserved.
+//   Licensed under the MIT License. See License-file in the project root for license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Soloplan.WhatsON.GUI.Common
+{
+  using System;
+  using System.Globalization;
+  using System.Windows.Data;
+
+  public class InvertBooleanConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return Negate(value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return Negate(value);
+    }
+
+    private static object Negate(object value)
+    {
+      if (value is bool boolean)
+      {
+        return !boolean;
+      }
+
+      return false;
+    }
+  }
+}
