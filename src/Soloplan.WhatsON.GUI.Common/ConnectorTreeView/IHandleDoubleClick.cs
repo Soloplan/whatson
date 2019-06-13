04dbc1fa3f4a5f5e76b5c24e9c8cd937b980d45e
DGO <d.golda@interia.eu>
2019-06-13 14:53:52 +0200

    Handling of double click will probably not change - remove ObsoleteAttribute.

--- src/Soloplan.WhatsON.GUI.Common/ConnectorTreeView/IHandleDoubleClick.cs
+++ src/Soloplan.WhatsON.GUI.Common/ConnectorTreeView/IHandleDoubleClick.cs
@@ -1,9 +1,7 @@
 ﻿namespace Soloplan.WhatsON.GUI.Common.ConnectorTreeView
 {
-  using System;
   using System.Windows.Input;
 
-  [Obsolete("This should be removed in favor of command binding. Not sure how though.")]
   public interface IHandleDoubleClick
   {
     void OnDoubleClick(object sender, MouseButtonEventArgs e);
