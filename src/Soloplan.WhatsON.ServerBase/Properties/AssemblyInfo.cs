f4c51b424b45cee8f16128ea93e7eaaad60450c2
Steffen Wilke <mail@steffen-wilke.de>
2019-08-21 14:53:31 +0200

    Remove the remaining files of the ServerBase project.

--- src/Soloplan.WhatsON.ServerBase/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Soloplan.WhatsON.Server")]
-[assembly: AssemblyDescription("The library that observers base for Server Build Jobs for the whatsON build and infrastructure monitoring tool.")]
