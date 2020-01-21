59f2a5f71893f27adb1e62a57b1017db0ee60951
DGO <d.golda@interia.eu>
2020-01-21 10:36:09 +0100

    Change display of jenkins builds. When build is running show progress/time to completion instead of time from build start.

--- src/Soloplan.WhatsON.GUI.Common/BuildServer/ProgressBarTooltipControl.xaml.cs
+++ src/Soloplan.WhatsON.GUI.Common/BuildServer/ProgressBarTooltipControl.xaml.cs
@@ -7,6 +7,7 @@
 
 namespace Soloplan.WhatsON.GUI.Common.BuildServer
 {
+  using System.Windows;
   using System.Windows.Controls;
 
   /// <summary>
@@ -14,9 +15,52 @@ namespace Soloplan.WhatsON.GUI.Common.BuildServer
   /// </summary>
   public partial class ProgressBarTooltipControl : UserControl
   {
+    /// <summary>
+    /// Dependency property for <see cref="CulpritsProp"/>.
+    /// </summary>
+    public static readonly DependencyProperty ControlOrientationProperty = DependencyProperty.Register(nameof(ControlOrientation), typeof(Orientation), typeof(ProgressBarTooltipControl), new PropertyMetadata(Orientation.Vertical));
+
+    public static readonly DependencyProperty CompactDisplayProperty = DependencyProperty.Register(nameof(CompactDisplay), typeof(bool), typeof(ProgressBarTooltipControl), new PropertyMetadata(false, new PropertyChangedCallback(OnCurrentReadingChanged)));
+
     public ProgressBarTooltipControl()
     {
       this.InitializeComponent();
+    }
+
+    private static void OnCurrentReadingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is ProgressBarTooltipControl control)
+      {
+        control.UpdateTexts();
+      }
+    }
+
+    public Orientation ControlOrientation
+    {
+      get => (Orientation)this.GetValue(ControlOrientationProperty);
+      set => this.SetValue(ControlOrientationProperty, value);
+    }
+
+    public bool CompactDisplay
+    {
+      get => (bool)this.GetValue(CompactDisplayProperty);
+      set => this.SetValue(CompactDisplayProperty, value);
+    }
+
+    public void UpdateTexts()
+    {
+      if (this.CompactDisplay)
+      {
+        this.CompletionText.Text = string.Empty;
+        this.EstimatedRemainingText.Text = " ETA";
+        this.PercentSignText.Text = "%/";
+      }
+      else
+      {
+        this.CompletionText.Text = "Completion: ";
+        this.EstimatedRemainingText.Text = " estimated remaining";
+        this.PercentSignText.Text = "% ";
+      }
     }
   }
 }
