95c5a0e317f50f1416a5a95a1671018c7b1b414e
Steffen Wilke <mail@steffen-wilke.de>
2020-02-10 16:37:38 +0100

    Bump version to 0.9.2.0 for the next release cycle.

--- config/SharedAssemblyInfo.cs
+++ config/SharedAssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Soloplan GmbH")]
-[assembly: AssemblyCopyright("Copyright © Soloplan GmbH 2019")]
+[assembly: AssemblyCopyright("Copyright © Soloplan GmbH 2020")]
 [assembly: AssemblyProduct("whatsON")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: AssemblyVersion("0.9.2.0")]
+[assembly: AssemblyFileVersion("0.9.2.0")]
