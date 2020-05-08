d4845c5cbb7952b457f3e0e474f12ccc4980296a
TSK <tomasz.skowron@soloplan.pl>
2020-05-08 16:13:38 +0200

    adding a converter

---
+++ src/Soloplan.WhatsON.GUI.Common/Converters/RGBAToRGBConverter.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullOrDefaultToVisibilityConverter.cs" company="Soloplan GmbH">
+//   Copyright (c) Soloplan GmbH. All rights reserved.
+//   Licensed under the MIT License. See License-file in the project root for license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Soloplan.WhatsON.GUI.Common.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+
+    /// <summary>
+    /// Converts RGBA hex with hash color to RGB color hex with hash
+    /// </summary>
+    internal class RGBAToRGBConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = (string)value;
+            string output = "";
+            for (int i = 0; i < 7; i++)
+            {
+                output += input[i];
+            }
+
+            return output;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
