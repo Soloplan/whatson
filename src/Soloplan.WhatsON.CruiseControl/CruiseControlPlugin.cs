5e0365ef4a9e871b531f7762232474c9a5af72e6
TSK <tomasz.skowron@soloplan.pl>
2020-05-19 11:26:56 +0200

    cc category fix

--- src/Soloplan.WhatsON.CruiseControl/CruiseControlPlugin.cs
+++ src/Soloplan.WhatsON.CruiseControl/CruiseControlPlugin.cs
@@ -51,7 +51,7 @@ namespace Soloplan.WhatsON.CruiseControl
           var serverProject = serverProjects.FirstOrDefault(s => s.Name == project.ServerName);
           if (serverProject == null)
           {
-            serverProject = new Project(null, project.ServerName, project.Name, address + "server/" + project.ServerName + "/project/" + project.Name);
+            serverProject = new Project(null, project.ServerName, address+"server/"+project.ServerName+"/viewServerReport.aspx", project.Name, address + "server/" + project.ServerName + "/project/" + project.Name);
             serverProjects.Add(serverProject);
             result.Add(serverProject);
           }
